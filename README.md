83b96ec767a03b58b06db2d03fbfdb3fe53739eb
root <nicolas.viallatte@gmail.com>
2019-02-14 18:28:34 +0100

    Init repository

---
+++ README.md
@@ -0,0 +1,88 @@
+# prometheus-jsonpath-exporter
+
+Converts json data from a Nexus 3 from endpoint `http://<nexus>/service/metrics/data`. This project is forked from https://github.com/project-sunbird/prometheus-jsonpath-exporter
+
+### Config
+
+```yml
+exporter_port: 9158 # Port on which prometheu can call this exporter to get metrics
+namespace: nexus
+log_level: debug|info|warn|error
+json_data_url: <url_with_scheme> # Url to get json data used for fetching metric values, and with the /service/metrics/data
+basic_auth_user: <username> # User to connect to Nexus. Optional, leave empty for empty Basic auth
+basic_auth_password: <password>
+metric_name_prefix: nexus # All metric names will be prefixed with this value
+metrics: # Default JSON metrics from /service/metrics/data
+- type: gauge
+  path: $.gauges # Object Path format to scrape gauges in the Nexus Json
+- type: meters
+  path: $.meters # Object Path format to scrape meters in the Nexus Json
+
+```
+### Run
+
+#### Using code (local)
+
+```
+# Ensure python 2.x and pip installed
+pip install -r app/requirements.txt
+python app/exporter.py example/config.yml
+```
+
+#### Using docker
+
+```
+docker run -p 9158:9158 -v $(pwd)/example/config.yml:/etc/prometheus-nexus-exporter/config.yml nviallatte/prometheus-nexus-exporter /etc/prometheus-nexus-exporter/config.yml
+```
+
+### Datas from Nexus
+
+This is a Json example retrieved from the Nexus 3 data endpoint. The `config.yml` file present in the Config part above allows you to parse the following Json in the Prometheus format.
+
+```json
+{
+	"version": "3.0.0",
+	"gauges": {
+		"jvm.buffers.direct.capacity": {
+			"value": 135636938
+		},
+		"jvm.buffers.direct.count": {
+			"value": 68
+    },
+    "..."
+  "meters": {
+		"com.sonatype.nexus.analytics.internal.ui.EventsComponent.clear.exceptions": {
+			"count": 0,
+			"m15_rate": 0.0,
+			"m1_rate": 0.0,
+			"m5_rate": 0.0,
+			"mean_rate": 0.0,
+			"units": "events/second"
+		},
+		"metrics.error": {
+			"count": 111,
+			"m15_rate": 8.904204078708139E-22,
+			"m1_rate": 4.5840990483670495E-274,
+			"m5_rate": 1.6245846951657174E-57,
+			"mean_rate": 9.885520410623464E-5,
+      "units": "events/second"
+    },
+    "..."
+  }
+}
+```
+
+### Usage
+
+Metrics will available in http://localhost:9158
+```
+$ curl -s localhost:9158 | grep ^nexus
+nexus_log_count{level="info"} 6387.0
+nexus_log_count{level="all"} 8297.0
+nexus_log_count{level="error"} 111.0
+nexus_response_count{status="3xx"} 1.0
+nexus_response_count{status="5xx"} 0.0
+nexus_response_count{status="2xx"} 78215.0
+[...]
+```
+
---
+++ app/exporter.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python
+
+import json
+import time
+import urllib2
+from prometheus_client import start_http_server
+from prometheus_client.core import GaugeMetricFamily, SummaryMetricFamily, REGISTRY
+import argparse
+import yaml
+from objectpath import Tree
+import logging
+import base64
+
+DEFAULT_PORT=9158
+DEFAULT_LOG_LEVEL='info'
+
+class JsonPathCollector(object):
+  def __init__(self, config):
+    self._config = config
+
+  def collect(self):
+    config = self._config
+    info_request = urllib2.Request(config['json_data_url'])
+    if config['basic_auth_user'] != "":
+      info_request.add_header("Authorization", "Basic %s" % base64.standard_b64encode('%s:%s' % (config['basic_auth_user'], config['basic_auth_password'])))
+    result = json.loads(urllib2.urlopen(info_request).read())
+    result_tree = Tree(result)
+    for metric_config in config['metrics']:
+      metric_path = metric_config['path']
+      metric_type = metric_config['type']
+      values = result_tree.execute(metric_path)
+
+      if metric_type == "gauge":
+        for current_gauge_metric_attribute in tuple(values):
+          current_gauge_metric_attribute_value = result_tree.execute(metric_path + ".*['" + current_gauge_metric_attribute + "'].value")
+          if isinstance(current_gauge_metric_attribute_value, float):
+            metric_name = "{}_{}".format(config['metric_name_prefix'], current_gauge_metric_attribute.replace('.', '_').replace('-','_').lower())
+            metric = GaugeMetricFamily(metric_name, '', value=current_gauge_metric_attribute_value)
+            yield metric
+
+      if metric_type == "meters":
+        exception_count_map = {}
+        for current_meters_metric_attribute in tuple(values):         
+          count_value=result_tree.execute(metric_path + ".*['" + current_meters_metric_attribute + "'].count")
+          exception_count_map[current_meters_metric_attribute] = count_value
+
+        for key in exception_count_map.keys():
+          suffix = key.replace('.', '_').replace('-','_').lower()
+          label = "type"
+          formatted_key = key
+          if ".exception" in key:
+            suffix = "exceptions"
+            label = "exceptionType"
+            formatted_key = key.replace(".exception", "")
+          elif "metrics." in key:
+            suffix = "log"
+            label = "level"
+            formatted_key = key.replace("metrics.", "")
+          elif "-responses" in key:
+            suffix = "response"
+            label = "status"
+            formatted_key = key.replace("-responses", "").replace("org.eclipse.jetty.webapp.WebAppContext.", "")
+          metric_name = "{}_{}".format(config['metric_name_prefix'], suffix)
+          metric = SummaryMetricFamily(metric_name, '', labels=[label])
+          metric.add_metric([formatted_key], count_value=exception_count_map.get(key), sum_value=exception_count_map.get(key))
+          yield metric
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(description='Expose metrics bu jsonpath for configured url')
+  parser.add_argument('config_file_path', help='Path of the config file')
+  args = parser.parse_args()
+  with open(args.config_file_path) as config_file:
+    config = yaml.load(config_file)
+    log_level = config.get('log_level', DEFAULT_LOG_LEVEL)
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.getLevelName(log_level.upper()))
+    exporter_port = config.get('exporter_port', DEFAULT_PORT)
+    logging.debug("Config %s", config)
+    logging.info('Starting server on port %s', exporter_port)
+    start_http_server(exporter_port)
+    REGISTRY.register(JsonPathCollector(config))
+  while True: time.sleep(1)
